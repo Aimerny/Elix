50c04728bbfb9ddd081dcd0114dcea2f058c4be5
Aimerny <bbbxingyu@gmail.com>
2024-04-11 00:56:47 +0800

    feat: init

--- src/main/java/icu/aimerny/elix/Elix.java
+++ src/main/java/icu/aimerny/elix/Elix.java
@@ -1,11 +1,12 @@
 package icu.aimerny.elix;
 
+import icu.aimerny.elix.registry.ModItem;
 import net.fabricmc.api.ModInitializer;
 
 public class Elix implements ModInitializer {
 
     @Override
     public void onInitialize() {
-
+        ModItem.init();
     }
 }
